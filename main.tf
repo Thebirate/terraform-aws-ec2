0b4e6ef45f2695d5a0c05c7821d526c394c142bf
Thebirate <60749357+Thebirate@users.noreply.github.com>
2025-10-07 22:35:46 -0700

    there were variables and tfvars when creating iac error fixed

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "ec2_instance" {
   ami           = var.ami_id
   instance_type = var.instance_type
   key_name      = var.key_pair_name
- 
+
 
   # Additional configuration like security groups, VPC, etc.
   # could be added here and referenced via variables.
--- outputs.tf
+++ outputs.tf
@@ -1,9 +1,14 @@
 output "instance_id" {
   description = "The ID of the created EC2 instance."
-  value       = aws_instance.main.*.id
+  value       = aws_instance.ec2_instance.id
 }
 
 output "instance_public_ip" {
   description = "The public IP address of the EC2 instance."
-  value       = aws_instance.main.*.public_ip
+  value       = aws_instance.ec2_instance.public_ip
+}
+
+output "instance_private_ip" {
+  description = "The private IP address of the EC2 instance."
+  value       = aws_instance.ec2_instance.private_ip
 }
